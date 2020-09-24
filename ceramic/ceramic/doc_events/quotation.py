5077fc74d3385b10d5266b2e5315a8a09f457930
finbyz <info@finbyz.com>
2020-09-24 18:07:08 +0530

    pull

--- ceramic/ceramic/doc_events/quotation.py
+++ ceramic/ceramic/doc_events/quotation.py
@@ -2,4 +2,4 @@ import frappe
 
 @frappe.whitelist()
 def get_items_from_item_group():
-    return frappe.get_list("Item Group",{'is_quotation_item':1}, order_by='quotation_index asc',ignore_permissions=True)
+    return frappe.get_list("Item Group",filters={'is_quotation_item':1},fields=['name','quotation_index','tile_size','tile_thickness','net_weight_per_box','tile_per_box','uom'],order_by='quotation_index asc',ignore_permissions=True)
--- ceramic/public/js/doctype_js/quotation.js
+++ ceramic/public/js/doctype_js/quotation.js
@@ -10,27 +10,21 @@ frappe.ui.form.on('Quotation', {
         })
     },
     get_items: function (frm) {
-        []
         frappe.call({
             method: "ceramic.ceramic.doc_events.quotation.get_items_from_item_group",
 
             callback: function (r) {
-                let d;
                 if (r.message) {
-                    console.log(r.message)
                     frm.doc.items = []
-                    r.message.forEach(function (item, index) {
-                        var row = frm.add_child("items");
-                        frappe.model.set_value(row.doctype, row.name, 'item_group', item.name)
-                        frappe.db.get_doc('Item Group', item.name).then(function (x) {
-                            frappe.model.set_value(row.doctype, row.name, 'quotation_index', x.quotation_index)
-                            frappe.model.set_value(row.doctype, row.name, 'tile_size', x.tile_size)
-                            frappe.model.set_value(row.doctype, row.name, 'tile_thickness', x.tile_thickness)
-                            frappe.model.set_value(row.doctype, row.name, 'net_weight_per_box', x.net_weight_per_box)
-                            frappe.model.set_value(row.doctype, row.name, 'tile_per_box', x.tile_per_box)
-                            frappe.model.set_value(row.doctype, row.name, 'uom', x.uom)
-
-                        });
+                        r.message.forEach(function (item) {
+                            var row = frm.add_child("items");
+                            frappe.model.set_value(row.doctype, row.name, 'item_group', item.name)
+                            frappe.model.set_value(row.doctype, row.name, 'quotation_index', item.quotation_index)
+                            frappe.model.set_value(row.doctype, row.name, 'tile_size', item.tile_size)
+                            frappe.model.set_value(row.doctype, row.name, 'tile_thickness', item.tile_thickness)
+                            frappe.model.set_value(row.doctype, row.name, 'net_weight_per_box', item.net_weight_per_box)
+                            frappe.model.set_value(row.doctype, row.name, 'tile_per_box', item.tile_per_box)
+                            frappe.model.set_value(row.doctype, row.name, 'uom', item.uom)
                     })
                     frm.refresh()
                 }
