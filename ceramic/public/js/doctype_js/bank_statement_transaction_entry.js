51b0fc296dcc5487aecd3829f7b0d30934acd0bc
finbyz <info@finbyz.com>
2020-09-24 06:52:16 +0000

    primary customer filter

--- ceramic/public/js/doctype_js/bank_statement_transaction_entry.js
+++ ceramic/public/js/doctype_js/bank_statement_transaction_entry.js
@@ -1,5 +1,5 @@
 this.frm.cscript.onload = function (frm) {
     this.frm.set_query("primary_customer", "new_transaction_items", function (doc) {
-        return { query: "erpnext.controllers.queries.customer_query" }
+        return { query: "ceramic.controllers.queries.new_customer_query" }
     });
 }
