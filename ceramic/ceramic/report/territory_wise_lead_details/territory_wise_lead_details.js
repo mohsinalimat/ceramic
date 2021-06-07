894c59380c0bd1ff04560d076ea69607c100bd0d
finbyz <info@finbyz.com>
2021-06-07 16:59:17 +0530

    new report lead detail

---
+++ ceramic/ceramic/report/territory_wise_lead_details/territory_wise_lead_details.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Finbyz and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Territory wise Lead Details"] = {
+	"filters": [
+
+	]
+};
