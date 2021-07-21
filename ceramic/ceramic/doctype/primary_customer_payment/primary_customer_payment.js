68a878b1c71d91226cff9308e48765dfba1e2b1c
finbyz <info@finbyz.com>
2021-07-21 11:27:49 +0530

    optimized change property on Mode Of Payment in Primary Customer Payment

--- ceramic/ceramic/doctype/primary_customer_payment/primary_customer_payment.js
+++ ceramic/ceramic/doctype/primary_customer_payment/primary_customer_payment.js
@@ -240,15 +240,15 @@ frappe.ui.form.on('Primary Customer Payment', {
 							frm.set_value(balance_field, r.message['account_balance']);
 
 							if(frm.doc.payment_type=="Receive" && currency_field=="paid_to_account_currency") {
-								frm.toggle_reqd(["reference_no", "reference_date"],
-									(r.message['account_type'] == "Bank" ? 1 : 0));
+								if (frm.doc.mode_of_payment !="Cash"){
+									frm.toggle_reqd(["reference_no", "reference_date"],(r.message['account_type'] == "Bank" ? 1 : 0));
+								}
 								if(!frm.doc.received_amount && frm.doc.paid_amount)
 									frm.events.paid_amount(frm);
 							}
 							// } else if(frm.doc.payment_type=="Pay" && currency_field=="paid_from_account_currency") {
 							// 	frm.toggle_reqd(["reference_no", "reference_date"],
 							// 		(r.message['account_type'] == "Bank" ? 1 : 0));
-
 							// 	if(!frm.doc.paid_amount && frm.doc.received_amount)
 							// 		frm.events.received_amount(frm);
 							// }
