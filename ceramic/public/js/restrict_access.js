8a418c521bdb8d28eb09ef2a1a4741a270b50094
finbyz <info@finbyz.com>
2021-08-26 16:59:36 +0530

    report party ledger ceramic js changes

--- ceramic/public/js/restrict_access.js
+++ ceramic/public/js/restrict_access.js
@@ -9,3 +9,14 @@ ceramic.restrict_access = {
         })
     }
 }
+if (frappe.user.has_role("Local Admin")){
+    frappe.db.get_value('Global Defaults', 'Global Defaults', 'restricted_access')
+        .then(r => {
+            let check = r.message.restricted_access;
+            if (check == 0){
+                $(window).load(function () {
+                    $("#toolbar-help").append('<a class="dropdown-item" href="#" onclick="return ceramic.restrict_access.restrict_access();">Restrict Access</a>');
+                });
+            }
+        });
+}
